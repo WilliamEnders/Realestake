52e218ff4786750e6f88f47e8eee19a06918b601
Hasibe Erdemir <erdeh815@newschool.edu>
2016-10-27 14:19:54 -0400

    spriteTest prefab complete

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class playerController : MonoBehaviour {
 
 	public Rigidbody rb;
-	public float moveSpeed = 10f;
+	public float moveSpeed;
 	public float jumpHeight;
 	public SpriteRenderer sr;
+	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		sr = GetComponent<SpriteRenderer> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -23,15 +25,23 @@ public class playerController : MonoBehaviour {
 			sr.flipX = false;
 		}
 
-		Vector2 move = new Vector2 (Input.GetAxis ("Horizontal"),Input.GetAxis ("Vertical")) * moveSpeed;
-
+		Vector2 move = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")) * moveSpeed;
 		Vector3 movement = new Vector3 (move.x, rb.velocity.y, move.y);
 		rb.velocity = movement;
-		 
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			//rb.velocity = new Vector2 (rb.velocity.x, 0);
-			rb.AddForce (Vector3.up * jumpHeight);
-			print ("jump");
+
+		if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0) {
+			anim.SetBool ("Walk", true);
+		}else if (Input.GetAxis ("Vertical") > 0 || Input.GetAxis ("Vertical") < 0) {
+			anim.SetBool ("Walk", true);
+		}else{ 
+			anim.SetBool ("Walk", false);
 		}
+		 
+		/*if (Input.GetKeyDown(KeyCode.Space)) {
+			rb.AddForce (Vector3.up * jumpHeight);
+			anim.SetBool ("Jump", true);
+		} else {
+			anim.SetBool ("Jump", false);
+		}*/
 	}
 }
