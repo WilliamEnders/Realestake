d6d74ab8a2a44290bd4347de1fd7ebb836b24fcd
Christina Goefron <cagoefron@gmail.com>
2016-10-27 14:30:32 -0400

    Added test score objects: ScoreController to collect score, ScoreGUI to display text/health bar, scoreTestCube object that tests player collisions

---
+++ Assets/christinaTestCrap/DisplayScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (Display.displays.Length > 1)
+			Display.displays[1].Activate();
+		if (Display.displays.Length > 2)
+			Display.displays[2].Activate();
+	}
+	
+
+}
---
+++ Assets/testScoreCollision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScoreCollision : MonoBehaviour {
+
+	public float scoreValue = 10f;     
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnColliderEnter(Collider other){
+		//if player.tag=realtor, press button, +score
+		if (other.tag == "Player"){
+			ScoreController.houseHealth += scoreValue;
+		}
+
+		if (other.tag == "Vampire"){
+			ScoreController.houseHealth -= scoreValue;
+		}
+
+
+		//if player.tag=vampire, press button, -score
+	}
+}
