dad1f9e01fe45b9b29613bde8a6e6035efc8d196
William Enders <endew231@newschool.edu>
2016-11-01 12:38:30 -0400

    walls disappear when you walk behind them, room manager

---
+++ Assets/roomManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roomManager : MonoBehaviour {
+
+	public int peopleInRoom;
+	public Renderer wall;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider info){
+		if(info.CompareTag("Player")){
+			peopleInRoom++;
+			WallTransparent (true);
+		}
+	}
+
+	void OnTriggerExit(Collider info){
+		if(info.CompareTag("Player")){
+			peopleInRoom--;
+			WallTransparent (false);
+		}
+	}
+
+	void WallTransparent(bool invisible){
+		if (invisible) {
+			wall.material.color = new Color (1, 1, 1, 0.5f);
+		} else {
+			wall.material.color = new Color (1, 1, 1, 1f);
+		}
+	}
+
+}
