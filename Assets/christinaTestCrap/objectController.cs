f7eec79385292099ba3e3785c07b8aa75314ce81
Christina Goefron <cagoefron@gmail.com>
2016-11-02 15:24:49 -0400

    Random breaking now functions; made a prefab with multiple cubes, which break/turn red.

--- Assets/christinaTestCrap/objectController.cs
+++ Assets/christinaTestCrap/objectController.cs
@@ -9,6 +9,8 @@ public class objectController : MonoBehaviour {
 	private float spawnTime; //random time interval
 	private float time; //current time
 	public GameObject[] repObjects;
+	public float breakPercent; //threshold # for percentage chance for breaking the object selected in loop
+	private float randomNumber;
 
 	//cap number of items that can be broken at a time (per level)
 	//may need to check day/night status to increase diffuclty per day or per time of day, etc.
@@ -19,34 +21,12 @@ public class objectController : MonoBehaviour {
 		SetRandomTime();
 		time = minTime;
 
-		//GameObject[] repObjects = new GameObject[repObjCount];
 
-
-		//for (int i = 1; i < repObjCount; i++) {
-
-			//if object is broken
-		repObjects = GameObject.FindGameObjectsWithTag ("repObject");
-
-		//Debug.Log (repObjects.Length);
-
-		//}
-
-		foreach(GameObject repairObj in repObjects)
-		{
-			//set logic for random break at scene load
-			//check objects' bool for broken/notbroken?
-
-			if(repairObj.GetComponent<objectHealthScript>().objectBroken == false){
-				//random number generator, check if number is above#
-				repairObj.GetComponent<objectHealthScript> ().objectBroken = true;
-				//exit loop ("break") if it is broken
-			}
-		}
 	}
 	
 
 	void Update () {
-	
+		//Debug.Log ("timer is " + spawnTime);
 	}
 
 	void FixedUpdate(){
@@ -54,7 +34,7 @@ public class objectController : MonoBehaviour {
 		//Counts up
 		time += Time.deltaTime; //reset timer when an object gets broken
 
-		//Check if its the right time to spawn the object
+		//Check if its the right time to break an object
 		if(time >= spawnTime){
 			RandomBreak();
 			SetRandomTime();
@@ -68,6 +48,42 @@ public class objectController : MonoBehaviour {
 	}
 
 	void RandomBreak(){ //break random #(variable) at random interval (variable)
-		time = minTime;
+
+		//load all breakable objects into array
+		repObjects = GameObject.FindGameObjectsWithTag ("repObject");
+
+		//Debug.Log (repObjects.Length);
+
+
+		foreach(GameObject repairObj in repObjects)
+		{
+			//set logic for random break at scene load
+			//check objects' bool for broken/notbroken?
+
+			if(repairObj.GetComponent<objectHealthScript>().objectBroken == false){
+				
+				//random number generator, check if number is above#
+				randomNumber = Random.Range(0f, 100f);
+
+
+				if (randomNumber <= breakPercent) {
+					repairObj.GetComponent<objectHealthScript> ().objectBroken = true;
+
+
+					Debug.Log ("object broken: " + randomNumber);
+
+					//exit loop ("break") if it is broken
+					break;
+
+				} else {
+					Debug.Log ("object skipped: " + randomNumber);
+				}
+
+
+			}
+		}
+
+		time = 0;
+
 	}
 }
