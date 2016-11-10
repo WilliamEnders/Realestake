fd35d36c74c7d4f789c59e92a0427efdf155b2fb
William Enders <endew231@newschool.edu>
2016-11-10 13:37:13 -0500

    start game

---
+++ Assets/startGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour {
+
+	public int playerId;
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		player = ReInput.players.GetPlayer(playerId);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player.GetAnyButtonDown()){
+			SceneManager.LoadScene (1);
+		}
+	}
+}
