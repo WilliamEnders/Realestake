dda6c50e9ab43cda6764421213689c2763aac560
Christina Goefron <cagoefron@gmail.com>
2016-11-12 12:23:50 -0500

    minor updates

--- Assets/startGame.cs
+++ Assets/startGame.cs
@@ -7,18 +7,26 @@ public class startGame : MonoBehaviour {
 
 	public int playerId;
 	public Player player;
+	private bool canStart;
 
 	// Use this for initialization
 	void Start () {
+		canStart = false;
 		player = ReInput.players.GetPlayer(playerId);
+		Invoke ("CanStart",16f);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(player.GetAnyButtonDown()){
+		if(player.GetAnyButtonDown() && canStart){
 			GetComponent<AudioSource>().Play();
 			SceneManager.LoadScene (1);
 		}
 	}
+
+	void CanStart(){
+		canStart = true;
+	}
+
 }
